11be4082f0c10e55c1975e61ca441e9f6f064ba2
ponap <42460009+ponap@users.noreply.github.com>
2019-01-29 17:25:48 -0600

    3 cuadros de colores

---
+++ practica2.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+ background(255,0,0);
+ stroke(0,255,0);
+ fill(0,255,0)
+ rect(133,0,133,400);
+ stroke(0,0,255);
+ fill(0,0,255);
+ rect(266,0,133,400);
+}
