4d6af1fb50b86f181b99472c924aeea96863fb3a
ponap <42460009+ponap@users.noreply.github.com>
2019-01-29 17:24:16 -0600

    Circulo con linea de negación

---
+++ practica3.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+ background(255,0,30);
+ strokeWeight(10);
+ stroke(255,117,20)
+ fill(255,0,30)
+ ellipse(200,200,100,100);
+ strokeWeight(10);
+ stroke(255,117,20);
+ line(230,230,170,170);
+}
